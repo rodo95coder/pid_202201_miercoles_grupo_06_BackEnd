78d50ac1ec9115311199c04dce08e9e386f86848
Pierodel-ruiz <pierodelgado68@gmail.com>
2022-05-04 19:00:22 -0500

    COommit piero

---
+++ java/com/PI/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.PI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.PI.entity.Departamento;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
+
+}
---
+++ java/com/PI/service/DepartamentoService.java
@@ -0,0 +1,14 @@
+package com.PI.service;
+
+import java.util.List;
+
+import com.PI.entity.Departamento;
+
+
+public interface DepartamentoService {
+	
+public List<Departamento> listaDepartamento();
+	
+	public Departamento insertaEdificio(Departamento obj);
+
+}
